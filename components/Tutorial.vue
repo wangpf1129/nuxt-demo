9f4517e6a5c7c6b9f830a1f6f8decf8c890240e3
Wangpf <1204981117@qq.com>
2021-11-30 12:25:20 +0800

    fix: server

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -51,7 +51,7 @@
         </svg>
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <h2 class="text-2xl leading-7 font-semibold">Hello nuxt</h2>
+        <h2 class="text-2xl leading-7 font-semibold">Hello nuxt demo</h2>
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the
           <a
