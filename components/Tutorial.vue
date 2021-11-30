8c01c5f70cbc7135b5c0e879ca5edef4205a3ba4
Wangpf <1204981117@qq.com>
2021-11-30 18:14:50 +0800

    perf: build dir nuxt-dist

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -52,7 +52,7 @@
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          test nuxt demo 1.1
+          test nuxt demo 1.2
         </h2>
     </div>
   </div>
