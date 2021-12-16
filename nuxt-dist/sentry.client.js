d26a16fed5e9962ae02539666091d4ef69b7c747
wwwpppfffzzz <66075776+wwwpppfffzzz@users.noreply.github.com>
2021-12-16 07:00:35 +0000

    Deploying to build from  @ c2fe87a96ff9781fa7fe5a2867686420befa5481 🚀

--- nuxt-dist/sentry.client.js
+++ nuxt-dist/sentry.client.js
@@ -10,7 +10,7 @@ export default function (ctx, inject) {
   const config = {
     dsn:"https:\u002F\u002F145a57e3050047269d79f9fad897254a@o1065687.ingest.sentry.io\u002F6096262",
     environment:"development",
-    release:"24fbe78bcdf01a31657bffa7738e41729046007a",
+    release:"c2fe87a96ff9781fa7fe5a2867686420befa5481",
     tracesSampleRate:1
   }
 
