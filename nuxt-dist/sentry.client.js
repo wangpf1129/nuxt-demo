d931265402a1c373e37a3ffb3e15c2d84f86d569
wangpf <wangpf2051@gmail.com>
2021-12-17 13:47:53 +0800

    test sentry sourcemap 1.3

--- nuxt-dist/sentry.client.js
+++ nuxt-dist/sentry.client.js
@@ -10,7 +10,7 @@ export default function (ctx, inject) {
   const config = {
     dsn:"https:\u002F\u002F145a57e3050047269d79f9fad897254a@o1065687.ingest.sentry.io\u002F6096262",
     environment:"development",
-    release:"c2fe87a96ff9781fa7fe5a2867686420befa5481",
+    release:"dbb537739e7e99a7f9fbfa1c3b64248f312c0ed6",
     tracesSampleRate:1
   }
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import SentryWebpackPlugin from "@sentry/webpack-plugin";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
@@ -42,6 +41,27 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/sentry',
   ],
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient) {
+        config.devtool = 'source-map'
+        const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            include: 'nuxt-dist/dist/client',
+            configFile: 'sentry.properties',
+            ignore: ['node_modules', 'webpack.config.js'],
+            urlPrefix: '~/',
+            cleanArtifacts: true,
+          })
+        )
+      }
+    },
+  },
   sentry: {
     dsn: 'https://145a57e3050047269d79f9fad897254a@o1065687.ingest.sentry.io/6096262',
     tracing: {
@@ -66,30 +86,6 @@ export default {
         attachProps: true, // 允许Sentry上报Vue组件Props
         logErrors: true, // 引入Sentry SDK后，默认不会将报错打印到控制台，将logErrors设为true强制将报错打印到控制台
       },
-    },
-  },
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extend(config, { isDev, isClient }) {
-      if (!isDev && isClient) {
-        config.devtool = 'source-map'
-        const release = 'demo-test1.0'
-        const SentryWebpackPlugin = require('@sentry/webpack-plugin')
-        config.plugins.push(
-          new SentryWebpackPlugin({
-            include: 'nuxt-dist/dist/client',
-            configFile: 'sentry.properties',
-            release,
-            debug: true,
-            ignore: ['node_modules', 'webpack.config.js'],
-            urlPrefix: '~/',
-            cleanArtifacts: true,
-          })
-        )
-      }
     },
   },
 }
