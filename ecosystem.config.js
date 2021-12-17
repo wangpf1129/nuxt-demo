e886299d87957c54a7c5ef428f4b2ce0209bfd13
wangpf <wangpf2051@gmail.com>
2021-12-17 11:38:02 +0800

    test deploy1.3

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [
+    {
+      name: 'nuxt-demo',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+    },
+  ],
+}
