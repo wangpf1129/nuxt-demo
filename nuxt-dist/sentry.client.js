db8d051ec5648c90dc4b5def3c205d7a8ed796b9
wangpf <wangpf2051@gmail.com>
2021-12-17 10:19:52 +0800

    test host

--- nuxt-dist/sentry.client.js
+++ nuxt-dist/sentry.client.js
@@ -10,7 +10,7 @@ export default function (ctx, inject) {
   const config = {
     dsn:"https:\u002F\u002F145a57e3050047269d79f9fad897254a@o1065687.ingest.sentry.io\u002F6096262",
     environment:"development",
-    release:"5bd63970a61c3e5134d7171140d6822aeee4d88a",
+    release:"c2fe87a96ff9781fa7fe5a2867686420befa5481",
     tracesSampleRate:1
   }
 
